e376dbc94f075a95cf0e29b18b4f4f211315199a
Yuvaraj <yuvarajlinux13@gmail.com>
2026-09-13 19:54:10 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from my Node.js DevOps application!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
